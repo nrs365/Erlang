61decfbd4779d665cb8c0da24367567823c8d2a4
nrs365 <nrs365@gmail.com>
2014-05-25 16:20:44 -0500

    added comments to practice function

--- erlang_practice.erl
+++ erlang_practice.erl
@@ -7,11 +7,11 @@
 %practice() -> %function declaration
 %F = 1.0. %float
 %S = "abc". %string
-%L = [1, 2, 3]. %list, i'm assuming, but it looks suspiciously like an array
-%L2 = [$a, $b, $c]. %still looks suspiciously like an array
+%L = [1, 2, 3]. %list, i'm assuming, but it looks suspiciously like an array %long not list(from C maybe)
+%L2 = [$a, $b, $c]. %still looks suspiciously like an array %double long (maybe)
 
 newfunction() ->
- io:fwrite("Practice function~n", []). %attempting to echo out something
+ io:fwrite("Practice function~n", []). %attempting to echo/fwrite out something %io - input/output?
 
 %to make it run type erlang_practice(the file name):new_function(name of the function)().
 %erlang_practice:new_function().
